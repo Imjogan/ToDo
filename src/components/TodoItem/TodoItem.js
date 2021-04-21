5eab8c153fcab8f8d837f0037a8db4fdf710ade3
Ivan Minin <wow.mjogan.official@yandex.ru>
2021-04-21 15:32:20 +0300

    добавлены checkbox

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,12 +1,20 @@
 import './TodoItem.css';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 function TodoItem({ unit, onDeleteClick, handleItemClick }) {
 
+  const [isCompleted, setCompleted] = useState(false);
+
+  const handleCheckboxChenge = (evt) => {
+    setCompleted(evt.target.checked);
+  }
+
   return (
     <div className="wrapper">
-      <li onClick={() => handleItemClick(unit)} className="todo-item">
-        <p className="todo-item__unit">{unit.title}</p>
+      <input onChange={handleCheckboxChenge} type="checkbox" className="todo-item__checkbox" />
+      <li onClick={() => handleItemClick(unit)} className={`todo-item ${isCompleted && 'todo-item_completed'}`}>
+        <p className={`todo-item__unit ${isCompleted && 'todo-item__unit_completed'}`}>{unit.title}</p>
       </li>
       <Button event={() => onDeleteClick(unit.id)} variant="delete" />
     </div>
